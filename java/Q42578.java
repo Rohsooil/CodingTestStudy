d20dcc5bbcf551df44fd1daeb6f8e60654fe0727
Rohsooil <food869@naver.com>
2022-04-12 22:23:07 +0900

    위장

---
+++ java/Q42578.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q42578 {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        }
+        int answer = 1;
+        for (String key : map.keySet()) {
+            answer *= (map.get(key) + 1);
+        }
+        return answer - 1;
+    }
+}
